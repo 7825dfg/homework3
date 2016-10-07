4cddb2cf533bf94fb36c67c93685ac51bb233655
7825dfg <7825dfg@naver.com>
2016-10-07 10:24:40 +0900

    B04 코딩완료

--- B04.java
+++ B04.java
@@ -1,1 +1,30 @@
 import java.util.*;
+
+ class B04 {
+     int sal;
+     double tax;
+   
+   void input(){
+     Scanner A = new Scanner(System.in);
+     System.out.println("연봉을 입력하시오.");
+     this.sal = A.nextInt();
+      output();
+   }
+   void output(){
+	 if(sal<10000000) this.tax = sal*0.095;
+	 else if(sal <40000000) this.tax = sal*0.19;
+	 else if(sal <80000000) this.tax = sal*0.28;
+	 else this.tax = sal*0.37;
+	 
+	 System.out.println("소득세는 "+tax+" 원 입니다.");
+	 
+   }
+   
+   
+   
+	public static void main(String[] args) {
+		new B04().input();
+
+	}
+
+}
