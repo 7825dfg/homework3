1b266e57c7a9818ccffb7fa619b4d8c8562f5e88
201244076 LDH <7825dfg@naver.com>
2016-10-12 18:16:51 +0900

    수업 미니퀴즈

---
+++ homework/src/homework/test.java
@@ -0,0 +1,31 @@
+package homework;
+
+import java.util.*;
+public class test {
+	 double sal;
+     double tax;
+   
+   void input(){
+     Scanner A = new Scanner(System.in);
+     System.out.println("연봉을 입력하시오.");
+     this.sal = A.nextInt();
+     output();
+   }
+   void output(){
+	 if(sal<10000000) this.tax = sal*0.095;
+	 else if(sal <40000000) this.tax = sal * 0.19;
+	 else if(sal <80000000) this.tax = sal * 0.28;
+	 else this.tax = sal*0.37;
+	 
+	 System.out.println("소득세는 "+(int)tax+" 원 입니다.");
+	 
+   }
+   
+   
+   
+	public static void main(String[] args) {
+		new test().input();
+
+	}
+
+}
